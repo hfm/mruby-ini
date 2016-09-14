8c1241c3b061be90f376f327b6a2a969ed1e3e4e
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-09-14 17:27:40 +0900

    Add test Ini#parse

--- test/mrb_ini.rb
+++ test/mrb_ini.rb
@@ -2,6 +2,21 @@
 ## Ini Test
 ##
 
+assert("Ini.#parse") do
+  expect = {
+    "section1" => {
+      "key" => "value",
+      "int" => 1,
+      "float" => 1.5,
+      "bool" => true
+    },
+  }
+
+  content = "[section1]\nkey=value\nint = 1\n    float      = 1.5\n\nbool=   true   "
+
+  assert_equal expect, Ini.parse(content)
+end
+
 assert("Ini.#load_file") do
   expect = {
     "DEFAULT" => {
